25d79e65a741d6f3c0ecd881b80b85c5e4dfbba4
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-17 16:08:28 -0400

    Parse and load prophet.json.

--- src/LinusShops/Prophet/Prophet.php
+++ src/LinusShops/Prophet/Prophet.php
@@ -11,13 +11,20 @@ namespace LinusShops\Prophet;
 
 class Prophet
 {
-    protected $modules;
+    protected $modules = array();
 
     /**
      * Loads a prophet.json, replacing whatever is currently held by this object
      * @param string $prophet parsed JSON
      */
     public function loadConfig($prophet)
+    {
+        if (isset($prophet['modules'])) {
+            $this->modules = $prophet['modules'];
+        }
+    }
+
+    public function run()
     {
 
     }
