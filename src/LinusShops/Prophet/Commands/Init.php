56b26693001af99c95d3fd62ae97ff36dec52f76
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-17 17:00:55 -0400

    Added command stubs.

---
+++ src/LinusShops/Prophet/Commands/Init.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Initializes a module with the expected structure for testing.
+ *
+ * @author Sam Schmidt
+ * @date 2015-04-17
+ * @company Linus Shops
+ */
+
+namespace LinusShops\Prophet\Commands;
+
+use LinusShops\Prophet\ProphetCommand;
+
+class Init extends ProphetCommand
+{
+
+}
---
+++ src/LinusShops/Prophet/Commands/Verify.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Command to verify that modules in prophet.json are all testable.
+ *
+ * @author Sam Schmidt
+ * @date 2015-04-17
+ * @company Linus Shops
+ */
+
+namespace LinusShops\Prophet\Commands;
+
+use LinusShops\Prophet\ProphetCommand;
+
+class Verify extends ProphetCommand
+{
+
+}
