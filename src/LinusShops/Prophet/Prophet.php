2fd902c9ca7a6cf31c3c2b0b218fccd32c63b88d
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-17 16:02:05 -0400

    Added handling to find and parse prophet.json.

---
+++ src/LinusShops/Prophet/Prophet.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 
+ *
+ * @author Sam Schmidt
+ * @date 2015-04-17
+ * @company Linus Shops
+ */
+
+namespace LinusShops\Prophet;
+
+class Prophet
+{
+    protected $modules;
+
+    /**
+     * Loads a prophet.json, replacing whatever is currently held by this object
+     * @param string $prophet parsed JSON
+     */
+    public function loadConfig($prophet)
+    {
+
+    }
+
+}
