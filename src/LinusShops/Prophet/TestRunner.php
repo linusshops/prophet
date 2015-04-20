78d72e6f1d16d75a9318887af04bcf1a72081750
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-20 12:37:18 -0400

    Look for phpunit.xml to load tests for a module.

--- src/LinusShops/Prophet/TestRunner.php
+++ src/LinusShops/Prophet/TestRunner.php
@@ -14,17 +14,19 @@ class TestRunner
     /**
      * Call the same functions used by the CLI PHPUnit
      * to engage the tests
-     * @param string $phpunitXml path to the phpunit.xml to use
+     * @param string $modulePath path to the phpunit.xml to use
      * @return int
      *      0: Tests successful
      *      1: Tests failed
      *      2: Failed with exception
      */
-    public function run($phpunitXml)
+    public function run($modulePath)
     {
         $runner = new \PHPUnit_TextUI_Command();
         $options = array(
-            '--configuration' => $phpunitXml
+            'phpunit',
+            '--configuration',
+            $modulePath.'/phpunit.xml'
         );
 
         return $runner->run($options, false);
