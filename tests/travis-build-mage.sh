b435bdef86ebc844896281a314250052899be5a4
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-30 11:29:14 -0400

    Updated travis install.

--- tests/travis-build-mage.sh
+++ tests/travis-build-mage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 composer install
-git clone git@github.com:OpenMage/magento-mirror.git magento
+git clone https://github.com/OpenMage/magento-mirror.git magento
 cd magento
 git checkout tags/1.9.1.0
 cd ..
