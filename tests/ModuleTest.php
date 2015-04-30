40edb126311ce4240bc45543a3b311e9c95ef66d
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-30 13:44:58 -0400

    Added module class test stubs.

---
+++ tests/ModuleTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * 
+ *
+ * @author Sam Schmidt
+ * @date 2015-04-30
+ * @company Linus Shops
+ */
+
+class ModuleTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetPath()
+    {
+
+    }
+
+    public function testGetName()
+    {
+
+    }
+
+    public function testValidate()
+    {
+
+    }
+
+    public function testCreateTestStructure()
+    {
+
+    }
+
+    public function testGetOption()
+    {
+
+    }
+
+    public function testIsIsolated()
+    {
+
+    }
+
+    public function testGetValidationErrors()
+    {
+
+    }
+}
