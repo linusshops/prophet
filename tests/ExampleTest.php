8c13f30b67cdb422d61f32d49ad67abe18f4ff8c
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-30 11:07:34 -0400

    Added example unit test.

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 
+ *
+ * @author Sam Schmidt
+ * @date 2015-04-30
+ * @company Linus Shops
+ */
+
+class ExampleTest extends PHPUnit_Framework_TestCase
+{
+    function testExample() {
+        $this->assertTrue(true);
+    }
+}
