2dfd19e2d700f448593a27b45db1517421baa7ee
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-30 13:38:33 -0400

    Notes on future prophet testing.

--- tests/test-build.sh
+++ tests/test-build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+##When it comes time to test prophet's testing functionality, we will need to
+##build an installation of magento. This script installs it from openmage,
+##and then loads our fixture module.
+
 composer install
 git clone https://github.com/OpenMage/magento-mirror.git magento
 cd magento && git checkout tags/1.9.1.0 && cd -
