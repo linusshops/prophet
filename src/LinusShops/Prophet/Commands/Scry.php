fa5201fc9c378dd1975b44b66bae4107d20ed23c
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-04-30 10:01:07 -0400

    Add cli option to only test a list of modules.
    Resolves #6.

--- src/LinusShops/Prophet/Commands/Scry.php
+++ src/LinusShops/Prophet/Commands/Scry.php
@@ -15,6 +15,7 @@ use LinusShops\Prophet\Module;
 use LinusShops\Prophet\ProphetCommand;
 use LinusShops\Prophet\TestRunner;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Scry extends ProphetCommand
@@ -25,12 +26,21 @@ class Scry extends ProphetCommand
 
         $this
             ->setName('scry')
-            ->setDescription('Run the test suites defined in prophet.json');
+            ->setDescription('Run the test suites defined in prophet.json')
+            ->addOption(
+                'module',
+                'm',
+                InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL,
+                'If set, prophet will only test modules matching the provided names.'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         parent::execute($input, $output);
+
+        $modulesRequested = $input->getOption('module');
+
         $moduleList = Config::getModuleList();
         if (empty($moduleList)) {
             $output->writeln('<error>No modules found in prophet.json.</error>');
@@ -42,12 +52,29 @@ class Scry extends ProphetCommand
             return;
         }
 
-        $output->writeln('Loading Magento classes');
+        if ($output->isVeryVerbose()) {
+            $output->writeln('Loading Magento classes');
+        }
 
         Magento::bootstrap();
 
+        if (count($modulesRequested)>0) {
+            $output->writeln('<info>Module list provided, will only test:</info>');
+
+            foreach ($modulesRequested as $requestedModule) {
+                $output->writeln('<info>    -- '.$requestedModule.'</info>');
+            }
+        }
+
         /** @var Module $module */
         foreach ($moduleList as $module) {
+            if (!in_array($module->getName(), $modulesRequested)) {
+                if ($output->isVerbose()) {
+                    $output->writeln('Skipping '.$module->getName());
+                }
+                continue;
+            }
+
             $output->writeln('Starting tests for ['.$module->getName().']');
 
             $runner = new TestRunner();
