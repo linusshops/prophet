fa6d7d32a5775eaf8ae93882ce63a2ed3f24f3f2
Sam Schmidt <samuel@canadasmotorcycle.ca>
2015-05-04 14:07:42 -0400

    Cleaned up scry command.

--- src/LinusShops/Prophet/Commands/Scry.php
+++ src/LinusShops/Prophet/Commands/Scry.php
@@ -11,6 +11,7 @@ namespace LinusShops\Prophet\Commands;
 
 use LinusShops\Prophet\Config;
 use LinusShops\Prophet\ConfigRepository;
+use LinusShops\Prophet\ConsoleHelper;
 use LinusShops\Prophet\Magento;
 use LinusShops\Prophet\Module;
 use LinusShops\Prophet\ProphetCommand;
@@ -21,6 +22,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Scry extends ProphetCommand
 {
+    /** @var ConsoleHelper  */
+    private $cli;
+
     /**
      * @return string
      */
@@ -32,6 +36,8 @@ class Scry extends ProphetCommand
     protected function configure()
     {
         parent::configure();
+
+        $this->cli = new ConsoleHelper();
 
         $this
             ->setName('scry')
@@ -56,64 +62,73 @@ class Scry extends ProphetCommand
     {
         $setupSuccessful = parent::execute($input, $output);
 
-        if (!$setupSuccessful) {
-            return;
-        }
+        if ($setupSuccessful) {
+            $config = ConfigRepository::getConfig();
+            $modulesRequested = $input->getOption('module');
 
-        $config = ConfigRepository::getConfig();
-        $modulesRequested = $input->getOption('module');
+            if ($this->loadClasses($modulesRequested, $config, $output)) {
+                /** @var Module $module */
+                foreach ($config->getModuleList() as $module) {
+                    if ($this->checkIfRequested($modulesRequested, $module, $output)) {
+                        if ($this->isIsolated($module, $input)) {
+                            $output->writeln("<info>Isolating {$module->getName()}</info>");
+                            $cmd = $this->getProphetCall() . " scry --isolated -m {$module->getName()}";
+                            $this->cli->veryVerbose($cmd, $output);
+                            passthru($cmd);
+                        } else {
+                            $output->writeln('Starting tests for [' . $module->getName() . ']');
 
-        if ($config->hasModules()) {
-            $output->writeln('<error>No modules found in prophet.json.</error>');
-
-            if ($output->isVeryVerbose()) {
-                $output->writeln(print_r($config->getModuleList(), true));
-            }
-
-            return;
-        }
-
-        if ($output->isVeryVerbose()) {
-            $output->writeln('Loading Magento classes');
-        }
-
-        Magento::bootstrap();
-
-        if (count($modulesRequested)>0 && $output->isVerbose()) {
-            $this->showRequestedModuleList($modulesRequested, $output);
-        }
-
-        /** @var Module $module */
-        foreach ($config->getModuleList() as $module) {
-            if (count($modulesRequested)>0 && !in_array($module->getName(), $modulesRequested)) {
-                if ($output->isVerbose()) {
-                    $output->writeln('Skipping '.$module->getName());
+                            $runner = new TestRunner();
+                            $runner->run($module->getPath());
+                        }
+                    }
                 }
-                continue;
-            }
-
-            if ($module->isIsolated() && !$input->getOption('isolated')) {
-                $output->writeln("<info>Isolating {$module->getName()}</info>");
-                $cmd = $this->getProphetCall()." scry --isolated -m {$module->getName()}";
-                if ($output->isVeryVerbose()) {
-                    $output->writeln($cmd);
-                }
-                passthru($cmd);
-            } else {
-                $output->writeln('Starting tests for ['.$module->getName().']');
-
-                $runner = new TestRunner();
-                $runner->run($module->getPath());
             }
         }
     }
 
+    private function loadClasses($modulesRequested, Config $config, OutputInterface $output)
+    {
+        $loaded = true;
+        if ($config->hasModules()) {
+            $output->writeln('<error>No modules found in prophet.json.</error>');
+            $loaded = false;
+        } else {
+            $this->cli->veryVerbose('Loading Magento classes...', $output);
+
+            Magento::bootstrap();
+
+            $this->showRequestedModuleList($modulesRequested, $output);
+        }
+
+        return $loaded;
+    }
+
+    private function isIsolated($module, InputInterface $input)
+    {
+        return $module->isIsolated() && !$input->getOption('isolated');
+    }
+
+    private function checkIfRequested($modulesRequested, $module, OutputInterface $output)
+    {
+        $requested = true;
+        if (count($modulesRequested)>0 && !in_array($module->getName(), $modulesRequested)) {
+            $this->cli->verbose('Skipping '.$module->getName(), $output);
+
+            $requested = false;
+        }
+
+        return $requested;
+    }
+
     private function showRequestedModuleList($modules, OutputInterface $output)
     {
-        $output->writeln('<info>Module list provided, will only test:</info>');
+        if (!$output->isVerbose() && count($modules)>0) {
+            $output->writeln('<info>Module list provided, will only test:</info>');
 
-        foreach ($modules as $requestedModule) {
-            $output->writeln('<info>    -- '.$requestedModule.'</info>');
+            foreach ($modules as $requestedModule) {
+                $output->writeln('<info>    -- '.$requestedModule.'</info>');
+            }
         }
     }
 }
---
+++ src/LinusShops/Prophet/ConsoleHelper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 
+ *
+ * @author Sam Schmidt
+ * @date 2015-05-04
+ * @company Linus Shops
+ */
+
+namespace LinusShops\Prophet;
+
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ConsoleHelper
+{
+    public function write($msg, OutputInterface $output)
+    {
+        $output->writeln($msg);
+    }
+
+    public function verbose($msg, $output)
+    {
+        if ($output->isVerbose()) {
+            $output->writeln($msg);
+        }
+    }
+
+    public function veryVerbose($msg, $output)
+    {
+        if ($output->isVeryVerbose()) {
+            $output->writeln($msg);
+        }
+    }
+}
