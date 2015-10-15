59d6f2df02c97904a2f38c674504f3d7b1ee261b
Sam Schmidt <samuel@dersam.net>
2015-10-15 12:22:04 -0400

    Check for phantom and selenium-server.

--- src/LinusShops/Prophet/Commands/Scry/Behat.php
+++ src/LinusShops/Prophet/Commands/Scry/Behat.php
@@ -13,6 +13,7 @@ namespace LinusShops\Prophet\Commands\Scry;
 use Behat\Behat\ApplicationFactory;
 use LinusShops\Prophet\Adapters\Behat\ProphetInput;
 use LinusShops\Prophet\Commands\Scry;
+use LinusShops\Prophet\ConsoleHelper;
 use LinusShops\Prophet\Module;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -35,12 +36,36 @@ class Behat extends Scry
         InputInterface $input,
         OutputInterface $output
     ) {
+        $cli = new ConsoleHelper();
         chdir($module->getPath());
+
+        //Confirm phantomjs and selenium server are running
+        if (!$this->checkProcess('phantomjs')) {
+            $cli->write('<error>phantomjs not running, exiting.</error>', $output);
+            return;
+        }
+
+        if (!$this->checkProcess('selenium-server')) {
+            $cli->write('<error>selenium-server not running, exiting.</error>', $output);
+            return;
+        }
+
         //Instantiate behat with a custom console Input to
         //avoid pollution from Prophet's cli input.
         $input = new ProphetInput(array());
 
         $factory = new ApplicationFactory();
         $factory->createApplication()->run($input);
+    }
+
+    public function checkProcess($name)
+    {
+        $running = false;
+        exec("pgrep {$name}", $output, $return);
+        if ($return == 0) {
+            $running = true;
+        }
+
+        return $running;
     }
 }
