2744707e762d15a1fd6a5e66b45821db3eeb15c5
Sam Schmidt <samuel@dersam.net>
2015-11-05 16:31:23 -0500

    Add plugin repository loading.

--- src/LinusShops/Prophet/PluginRepository.php
+++ src/LinusShops/Prophet/PluginRepository.php
@@ -14,19 +14,26 @@ class PluginRepository
 {
     private static $plugins = array();
 
-    /**
-     *
-     * @param $name string Plugin name (the repo name, minus the prophet-plugin-)
-     * @return Plugin
-     */
-    public static function get($name)
+    public static function load()
     {
-        $plugin = null;
+        $base = dirname(ConfigRepository::getProphetPath()).'/plugins';
 
-        if (isset(self::$plugins[$name])) {
-            $plugin = self::$plugins[$name];
-        } else {
-            
+        foreach (scandir($base) as $pluginDir) {
+            if (strpos($pluginDir, 'prophet-plugin-') !== false) {
+                $path = $base.'/'.$pluginDir.'/plugin.php';
+                if (file_exists($path)) {
+                    $name = str_replace('prophet-plugin-', '', $pluginDir);
+                    /** @var Plugin $plugin */
+                    $plugin = require($path);
+                    $plugin->load();
+                    $plugin->register();
+                    self::$plugins[$name] = $plugin;
+                }
+            }
         }
+    }
+
+    public function get($name) {
+        return self::$plugins[$name];
     }
 }
