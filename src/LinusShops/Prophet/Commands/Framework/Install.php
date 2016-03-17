ed52f84fbafb26bc86bfe9bf12e41a7971ecffde
Sam Schmidt <samuel@dersam.net>
2016-03-17 09:58:37 -0400

    Fix incomplete previous commit.

--- src/LinusShops/Prophet/Commands/Framework/Install.php
+++ src/LinusShops/Prophet/Commands/Framework/Install.php
@@ -27,8 +27,9 @@ class Install extends Command
             )
             ->addArgument(
                 'repository',
-                InputArgument::REQUIRED,
-                'Url to git repository'
+                InputArgument::OPTIONAL,
+                'Url to git repository',
+                null
             )
         ;
     }
@@ -49,7 +50,25 @@ class Install extends Command
             return;
         }
 
-        $this->shell('git clone '.$input->getArgument('repository').' '.$name, $frameworkDir);
-        $this->shell('composer install', $frameworkDir.'/'.$name);
+        $repo = $input->getArgument('repository');
+
+        if (empty($repo)) {
+            $output->writeln("Repository not provided, checking defaults...");
+            $defaultRepos = file_get_contents(PROPHET_ROOT_DIR.'/defaults/frameworks.json');
+            $defaultRepos = json_decode($defaultRepos, true);
+            if ($defaultRepos && isset($defaultRepos[$name])) {
+                $repo = $defaultRepos[$name]['url'];
+                $output->writeln("Using {$repo}...");
+            } else {
+                $output->writeln("{$name} has no default repository.");
+                return;
+            }
+        }
+
+        if ($this->shell('git clone '.$repo.' '.$name, $frameworkDir)) {
+            $this->shell('composer install', $frameworkDir . '/' . $name);
+        } else {
+            $output->writeln('Failed to install from '.$repo);
+        }
     }
 }
