ccae43c16f49ae66061c7b104ebd83c1311aac69
Sam Schmidt <samuel@dersam.net>
2016-03-17 10:56:33 -0400

    Clean up plugin loader.

--- src/LinusShops/Prophet/PluginRepository.php
+++ src/LinusShops/Prophet/PluginRepository.php
@@ -9,35 +9,33 @@
 
 namespace LinusShops\Prophet;
 
-
 class PluginRepository
 {
     private static $plugins = array();
 
     public static function load()
     {
-        $base = dirname(ConfigRepository::getProphetPath()).'/plugins';
+        $base = PROPHET_ROOT_DIR.'/plugins';
 
-        foreach (scandir($base) as $pluginDir) {
-            if (strpos($pluginDir, 'prophet-plugin-') !== false) {
-                $path = $base.'/'.$pluginDir.'/plugin.php';
-                if (file_exists($path)) {
-                    $name = str_replace('prophet-plugin-', '', $pluginDir);
-                    /** @var Plugin $plugin */
-                    $plugin = require($path);
-                    $plugin->load();
-                    $plugin->register();
-                    self::$plugins[strtolower($name)] = $plugin;
-                }
+        foreach (scandir($base) as $name) {
+            $path = $base.'/'.$name.'/plugin.php';
+            if (file_exists($path)) {
+                /** @var Plugin $plugin */
+                $plugin = require($path);
+                $plugin->load();
+                $plugin->register();
+                self::$plugins[strtolower($name)] = $plugin;
             }
         }
     }
 
-    public static function get($name) {
+    public static function get($name)
+    {
         return self::$plugins[strtolower($name)];
     }
 
-    public static function has($name) {
+    public static function has($name)
+    {
         return isset(self::$plugins[strtolower($name)]);
     }
 }
