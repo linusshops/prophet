3f41790c5a070d8a0867daef40be2059127eb9bd
Samuel Schmidt <samuel@dersam.net>
2016-04-13 12:29:55 -0400

    #61: Build script for test environment.

---
+++ make_test_environment.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone --branch magento-1.9 --depth 1 https://github.com/OpenMage/magento-mirror.git testbed
+cp ./tests/composer.magento.json ./testbed/composer.json
+cd testbed && composer install && composer run-script post-install-cmd -vvv -- --redeploy && cd -
