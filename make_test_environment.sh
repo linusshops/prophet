a75389fb40d86d3bdd432ef4e2aaa63b06e892db
Samuel Schmidt <samuel@dersam.net>
2016-04-13 12:33:39 -0400

    #61: prophet config for test environment.

--- make_test_environment.sh
+++ make_test_environment.sh
@@ -2,4 +2,5 @@
 
 git clone --branch magento-1.9 --depth 1 https://github.com/OpenMage/magento-mirror.git testbed
 cp ./tests/composer.magento.json ./testbed/composer.json
+cp ./tests/prophet.magento.yml ./testbed/prophet.yml
 cd testbed && composer install && composer run-script post-install-cmd -vvv -- --redeploy && cd -
